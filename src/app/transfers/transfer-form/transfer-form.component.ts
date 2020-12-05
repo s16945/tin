ca6a30d64562920bbef6b681d15094608ac3e5bb
Marcin Pejski <marcin.pejski@connectpoint.pl>
2020-12-05 17:36:33 +0100

    added frontend forms validation

--- src/app/transfers/transfer-form/transfer-form.component.ts
+++ src/app/transfers/transfer-form/transfer-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {
+  dateAfterOtherDateValidator,
+  dateValidator,
+  numberRangeValidator,
+  numberValidator,
+  selectionValidator
+} from '../../util/form/validator/common-validator';
 
 @Component({
   selector: 'app-transfer-form',
@@ -6,9 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferFormComponent implements OnInit {
 
-  constructor() { }
+  transferForm: FormGroup;
+  contractDatesGroup: FormGroup;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.contractDatesGroup = new FormGroup({
+      contractStartDate: new FormControl('', [Validators.required, dateValidator()]),
+      contractEndDate: new FormControl('', [Validators.required, dateValidator()]),
+    }, [dateAfterOtherDateValidator('contractStartDate', 'contractEndDate')]);
+
+    this.transferForm = new FormGroup({
+      athleteName: new FormControl('',
+        [Validators.required, selectionValidator()]),
+      managerName: new FormControl('', [Validators.required, selectionValidator()]),
+      currentClub: new FormControl('', [Validators.required]),
+      newClub: new FormControl('', [Validators.required, selectionValidator()]),
+      transferDate: new FormControl('', [Validators.required, dateValidator()]),
+      price: new FormControl('', [Validators.required, numberValidator()]),
+      commission: new FormControl('', [Validators.required, numberRangeValidator(0, 100)]),
+      isLoan: new FormControl(''),
+      contractDatesGroup: this.contractDatesGroup
+    });
+  }
+
+  onSubmit() {
+    this.transferForm.markAllAsTouched();
+
+    // do submit
+  }
 }
---
+++ src/app/util/form/validator/common-validator.ts
@@ -0,0 +1,79 @@
+import {AbstractControl, ValidatorFn} from '@angular/forms';
+
+export function numberValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (!control.value) {
+      return {invalidNumber: true};
+    }
+
+    return !isNaN(control.value) ? null : {invalidNumber: true};
+  };
+}
+
+export function dateValidator() {
+  const pattern = /(\d{4})-(\d{2})-(\d{2})/;
+
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    return pattern.test(control.value) ? null : {invalidDate: true};
+  };
+}
+
+export function selectionValidator() {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    return control.value ? null : {invalidSelection: true};
+  };
+}
+
+export function numberRangeValidator(min, max) {
+
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const errorObj = {outOfRange: true};
+
+    if (!control.value) {
+      return null;
+    }
+
+    if (isNaN(control.value)) {
+      return errorObj;
+    }
+
+    const value = parseFloat(control.value);
+    if (value < min) {
+      return errorObj;
+    }
+    return value <= max ? null : errorObj;
+  };
+}
+
+export function dateAfterOtherDateValidator(dateFieldName1, dateFieldName2) {
+  return (controlGroup: AbstractControl): { [key: string]: any } | null => {
+    if (!controlGroup.get(dateFieldName1).value || !controlGroup.get(dateFieldName2).value) {
+      return null;
+    }
+
+    const errorObj = {dateAfter: true};
+    const date1 = controlGroup.get(dateFieldName1).value;
+    const date2 = controlGroup.get(dateFieldName2).value;
+
+    if (!date1) {
+      return null;
+    }
+    if (!date2) {
+      return null;
+    }
+
+    const pattern = /(\d{4})-(\d{2})-(\d{2})/;
+    if (!pattern.test(date1)) {
+      return errorObj;
+    }
+
+    if (!pattern.test(date2)) {
+      return errorObj;
+    }
+
+    const valueDate = new Date(date1);
+    const compareToDate = new Date(date2);
+    return valueDate.getTime() >= compareToDate.getTime() ? errorObj : null;
+  };
+}
+
